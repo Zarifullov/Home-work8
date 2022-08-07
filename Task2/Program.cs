9ed6ac89cf09d1c746d2c8ce29708e3c7e43eadb
Renat <rzarifullov@gmail.com>
2022-08-07 18:54:38 +0300

    Task2 added

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -11,3 +11,54 @@
 // 5 2 6 7
 
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+Console.Write("Введите размер квадратного массива: ");
+int size = int.Parse(Console.ReadLine());
+
+int[,] array = new int[size, size];
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum = sum + array[i, j];        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine("Строка с наименьшей суммой элементов под номером: " + (indexLine) + ", сумма элементов = " + (minsum));
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
