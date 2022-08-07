160307aed0ffa79f090ea10aa5de1c09055ef0cb
Renat <rzarifullov@gmail.com>
2022-08-07 19:26:47 +0300

    Task3 added

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,7 +7,47 @@
 
 
 
-Console.Write($"{array[i,j] / 10} {array[i,j] % 10 }")
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+
+int[,] array = new int[size, size];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
 {
-    
+    array[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1)
+        ++j;
+    else 
+    if (i < j && i + j >= size - 1)
+        ++i;
+    else 
+    if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++num;
+}
+
+PrintArray(array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            
+            if (array[i,j] / 10 <= 0)
+            Console.Write($" {array[i, j]} ");
+
+            else Console.Write($"{array[i, j]} ");
+        }
+        
+        Console.WriteLine();
+    }
 }
